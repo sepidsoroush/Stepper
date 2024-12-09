66cc8adc61440cf3e07d340396aa4aa1837c5a2a
sepidsoroush <s.soroush2012@gmail.com>
2024-12-09 16:28:32 +0200

    add a confetti to last step

--- src/components/multi-step-form.tsx
+++ src/components/multi-step-form.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import { Button } from "./ui/button";
+import Confetti from "./ui/confetti";
 import DummyContent from "./dummy-content";
 import StepperIndicator from "./stepper-indicator";
+import { RotateCcw } from "lucide-react";
 
 function getStepContent(step: number) {
   switch (step) {
@@ -19,7 +21,8 @@ function getStepContent(step: number) {
 
 const MultiStepForm = () => {
   const [activeStep, setActiveStep] = useState(1);
-  const [direction, setDirection] = useState<"left" | "right">("left"); // Track direction
+  const [direction, setDirection] = useState<"left" | "right">("left");
+  const [isCompleted, setIsCompleted] = useState(false);
 
   const handleNext = async () => {
     setDirection("left");
@@ -31,48 +34,76 @@ const MultiStepForm = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
+  const handleDone = () => {
+    setIsCompleted(true);
+  };
+
+  const handleRestart = () => {
+    setActiveStep(1);
+    setIsCompleted(false);
+  };
+
   return (
     <div className="border p-4 rounded-xl dark:border-secondary/50 overflow-hidden">
-      <StepperIndicator activeStep={activeStep} />
-      <motion.div
-        key={activeStep}
-        initial={{ x: direction === "left" ? 300 : -300 }}
-        animate={{ x: 0 }}
-        exit={{ x: direction === "left" ? -300 : 300 }}
-        transition={{
-          type: "tween",
-          ease: "easeOut",
-          duration: 0.3,
-        }}
-      >
-        {getStepContent(activeStep)}
-      </motion.div>
-      <div className="flex flex-row gap-4 justify-center items-center">
-        <Button
-          variant="outline"
-          className="w-24 rounded-3xl dark:bg-foreground dark:text-secondary dark:hover:bg-primary/90 dark:hover:text-secondary"
-          onClick={handleBack}
-          disabled={activeStep === 1}
-        >
-          Back
-        </Button>
-        {activeStep === 3 ? (
-          <Button
-            className="w-24 rounded-3xl dark:bg-secondary dark:text-primary dark:hover:bg-secondary/90"
-            onClick={() => console.log("Completed!")}
-          >
-            Done
-          </Button>
-        ) : (
+      {isCompleted ? (
+        <div className="relative flex flex-col flex-wrap justify-center items-center">
+          <Confetti />
+          <div className="absolute top-0 mt-4 text-md dark:text-secondary text-center">
+            I hope you have fun using this component. Please feel free to reach
+            me at s.soroush2012@gmail.com
+          </div>
           <Button
             variant="outline"
-            className="w-24 rounded-3xl dark:bg-foreground dark:text-secondary dark:hover:bg-primary/90 dark:hover:text-secondary"
-            onClick={handleNext}
+            className="z-10 w-10 p-4 rounded-full dark:bg-foreground dark:text-secondary dark:hover:bg-primary/90 dark:hover:text-secondary"
+            onClick={handleRestart}
           >
-            Next
+            <RotateCcw />
           </Button>
-        )}
-      </div>
+        </div>
+      ) : (
+        <div>
+          <StepperIndicator activeStep={activeStep} />
+          <motion.div
+            key={activeStep}
+            initial={{ x: direction === "left" ? 300 : -300 }}
+            animate={{ x: 0 }}
+            exit={{ x: direction === "left" ? -300 : 300 }}
+            transition={{
+              type: "tween",
+              ease: "easeOut",
+              duration: 0.3,
+            }}
+          >
+            {getStepContent(activeStep)}
+          </motion.div>
+          <div className="flex flex-row gap-4 justify-center items-center">
+            <Button
+              variant="outline"
+              className="w-24 rounded-3xl dark:bg-foreground dark:text-secondary dark:hover:bg-primary/90 dark:hover:text-secondary"
+              onClick={handleBack}
+              disabled={activeStep === 1}
+            >
+              Back
+            </Button>
+            {activeStep === 3 ? (
+              <Button
+                className="w-24 rounded-3xl dark:bg-secondary dark:text-primary dark:hover:bg-secondary/90"
+                onClick={handleDone}
+              >
+                Done
+              </Button>
+            ) : (
+              <Button
+                variant="outline"
+                className="w-24 rounded-3xl dark:bg-foreground dark:text-secondary dark:hover:bg-primary/90 dark:hover:text-secondary"
+                onClick={handleNext}
+              >
+                Next
+              </Button>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/components/ui/confetti.tsx
@@ -0,0 +1,93 @@
+import type { ReactNode } from "react";
+import React, {
+  createContext,
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useMemo,
+  useRef,
+} from "react";
+import type {
+  GlobalOptions as ConfettiGlobalOptions,
+  CreateTypes as ConfettiInstance,
+  Options as ConfettiOptions,
+} from "canvas-confetti";
+import confetti from "canvas-confetti";
+
+type Api = {
+  fire: (options?: ConfettiOptions) => void;
+};
+
+type Props = React.ComponentPropsWithRef<"canvas"> & {
+  options?: ConfettiOptions;
+  globalOptions?: ConfettiGlobalOptions;
+  children?: ReactNode;
+};
+
+export type ConfettiRef = Api | null;
+
+const ConfettiContext = createContext<Api>({} as Api);
+
+const Confetti = forwardRef<ConfettiRef, Props>((props, ref) => {
+  const {
+    options,
+    globalOptions = { resize: true, useWorker: true },
+    children,
+    ...rest
+  } = props;
+  const instanceRef = useRef<ConfettiInstance | null>(null);
+  const mountedRef = useRef(false);
+
+  const canvasRef = useCallback(
+    (node: HTMLCanvasElement) => {
+      if (node !== null) {
+        if (instanceRef.current) return;
+        instanceRef.current = confetti.create(node, {
+          ...globalOptions,
+          resize: true,
+        });
+        mountedRef.current = true;
+      } else {
+        if (instanceRef.current) {
+          instanceRef.current.reset();
+          instanceRef.current = null;
+        }
+        mountedRef.current = false;
+      }
+    },
+    [globalOptions]
+  );
+
+  const fire = useCallback(
+    (opts = {}) => instanceRef.current?.({ ...options, ...opts }),
+    [options]
+  );
+
+  const api = useMemo(
+    () => ({
+      fire,
+    }),
+    [fire]
+  );
+
+  useImperativeHandle(ref, () => api, [api]);
+
+  useEffect(() => {
+    if (mountedRef.current && instanceRef.current) {
+      fire();
+    }
+  }, [fire]);
+
+  return (
+    <ConfettiContext.Provider value={api}>
+      <canvas ref={canvasRef} {...rest} />
+      {children}
+    </ConfettiContext.Provider>
+  );
+});
+
+Confetti.displayName = "Confetti";
+
+export { Confetti, ConfettiContext };
+export default Confetti;
